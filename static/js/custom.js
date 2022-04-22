d67318d7495199ad7a33ca4bd38a1a790ab9c500
zhyea <zhangrui@zhyea.com>
2022-04-22 21:50:47 +0800

    1. 实现动态居中 2. 添加菜单
    Change-Id: I0869b0c41d2cc77cba1d0a8d72a8cfe2cf92d28c

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,13 @@
-window.addEventListener('resize', function () {
-    //let width =
+window.addEventListener('resize', middleContentBody);
+window.addEventListener('load', middleContentBody);
+
+/**
+ * 使contentBody块动态居中
+ */
+function middleContentBody() {
     console.log(document.body.clientWidth);    //浏览器时下窗口文档body的高度
-});
+    let bodyWidth = document.body.clientWidth;
+    let valMargin = Math.floor((bodyWidth % 324) / 2);
+    let domContent = document.getElementById("content-body");
+    domContent.style.cssText = ('margin: 0 ' + valMargin + 'px');
+}

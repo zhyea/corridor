afdad24c18572d22a87a79503ffcefd4c1b81465
zhyea <zhangrui@zhyea.com>
2022-04-23 22:50:55 +0800

    1. 页面效果调整
    Change-Id: I439e0f46060a1901673867652c6a52da520dfea5

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,8 +6,9 @@ window.addEventListener('load', middleContentBody);
  */
 function middleContentBody() {
     console.log(document.body.clientWidth);    //浏览器时下窗口文档body的高度
-    let bodyWidth = document.body.clientWidth;
-    let valMargin = Math.floor((bodyWidth % 324) / 2);
+    let bodyWidth = document.body.clientWidth - 49;
+    let valMargin = Math.floor((bodyWidth % 316) / 2);
     let domContent = document.getElementById("content-body");
+    console.log(valMargin);
     domContent.style.cssText = ('margin: 0 ' + valMargin + 'px');
 }

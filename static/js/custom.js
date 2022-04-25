6425c1b35e0113de816b43011e5654c9365d428c
zhangrui137 <zhangrui137@ke.com>
2022-04-25 11:36:56 +0800

    1.添加宽度识别; 2.样式微调

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,7 +4,7 @@ window.addEventListener('load', resizeContentBody);
 function resizeContentBody() {
     //console.log(document.body.clientWidth);    //浏览器时下窗口文档body的高度
     let bodyWidth = document.body.clientWidth;
-    bodyWidth = bodyWidth > 1300 ? 1300 - 2 : bodyWidth - 2
+    bodyWidth = bodyWidth > 1600 ? 1600 - 2 : bodyWidth - 2
     let valMargin = Math.floor((bodyWidth % 316) / 2);
     //console.log(valMargin);
     let domContent = document.getElementById("content-body");

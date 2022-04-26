5306aa71572caf1e6c98e9186c5d2d8d5bc7af30
zhyea <zhangrui@zhyea.com>
2022-04-26 10:50:32 +0800

    文章页样式调整

--- static/js/custom.js
+++ static/js/custom.js
@@ -10,3 +10,11 @@ function resizeContentBody() {
     let domContent = document.getElementById("content-body");
     domContent.style.cssText = ('margin: 0 ' + valMargin + 'px');
 }
+
+
+function socialButtons() {
+    document.writeln('Button');
+    //<a href="javascript:window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent('您的链接')+'&t='+encodeURIComponent(document.title),'_blank','toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=600, height=450,top=100,left=350');void(0)" style="font-size: 14px;margin-right: 50px;">Facebook </a>
+    //<a href="javascript:window.open('http://twitter.com/share?url=' + encodeURIComponent('您的链接') + '&text=' + encodeURIComponent('标题'), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');void(0)"  style="font-size: 14px;margin-right: 50px;">Twitter</a>
+    //<a href="javascript:window.open('https://www.pinterest.com/pin/create/button/?url='+encodeURIComponent('https://imgconvert.csdnimg.cn/aHR0cHM6Ly91c2VyLWdvbGQtY2RuLnhpdHUuaW8vMjAxNy85LzQvYzQyNzA0NzNjNDJjNGE1ZDE0ZWI0NzRjOGQ5NTcwZWI_aW1hZ2VWaWV3Mi8wL3cvMTI4MC9oLzk2MC9mb3JtYXQvd2VicC9pZ25vcmUtZXJyb3IvMQ'),'_blank','toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=600, height=450,top=100,left=350');void(0)" onclick="window.open(this.href,'','scrollbars=1,resizable=1,width=600,height=450,top=100,left=350');return false;" style="font-size: 14px;margin-right: 48px;">Pinterest</a>
+}

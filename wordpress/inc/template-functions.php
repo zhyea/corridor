123d8b2793bdd5fc695d0f56d45626c8a547918d
zhyea <zhangrui@zhyea.com>
2022-06-01 20:55:52 +0800

    调整摘要长度
    Change-Id: Icaf4fa732c84e50950dd5a5b3bfd0875c08740a1

--- wordpress/inc/template-functions.php
+++ wordpress/inc/template-functions.php
@@ -22,3 +22,27 @@ function corridor_excerpt_length( $length ) {
 }
 
 add_filter( 'excerpt_length', 'corridor_excerpt_length' );
+
+
+/**
+ * Limit excerpt to a number of characters
+ *
+ * @param string $excerpt
+ *
+ * @return string
+ */
+function corridor_short_excerpt( $excerpt ) {
+	// Get excerpt length from database.
+	$excerpt_length = get_corridor_option( 'excerpt_length' );
+
+	// Return excerpt text.
+	if ( $excerpt_length >= 0 ) :
+		$excerpt_length = absint( $excerpt_length );
+	else :
+		$excerpt_length = 35; // Number of words.
+	endif;
+
+	return substr( $excerpt, 0, $excerpt_length );
+}
+
+add_filter( 'the_excerpt', 'corridor_short_excerpt' );
